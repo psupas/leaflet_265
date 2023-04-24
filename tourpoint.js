b7312ca251b359c770f8ad598934474eec353548
psupas <124566296+psupas@users.noreply.github.com>
2023-04-24 11:34:08 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วัดพระธาตุดอยสุเทพ",
+        "type": "culture",
+        "level": "💗💗💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.9219866766129,
+          18.805009425808564
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ดอยอินทนนท์",
+        "type": "natural",
+        "level": "💗💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.48691849999744,
+          18.590797852030477
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ประตูท่าแพ",
+        "type": "culture",
+        "level": "💗💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.9935487288351,
+          18.78791441459063
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ม่อนแจ่ม",
+        "type": "natural",
+        "level": "💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.82269599999944,
+          18.935554037670357
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วัดอุโมงค์",
+        "type": "culture",
+        "level": "💗💗💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.95272299999931,
+          18.783555074398194
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ดอยหลวงเชียงดาว",
+        "type": "natural",
+        "level": "💗💗💗💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.96404999999834,
+          19.362975853888955
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "บ้านแม่กำปอง",
+        "type": "natural",
+        "level": "💗💗💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          99.35013253956384,
+          18.865430799302956
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "คลองแม่ข่า",
+        "type": "culture",
+        "level": "💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.99874000000312,
+          18.780885260798954
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วันนิมมาน",
+        "type": "special",
+        "level": "💗💗💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.96778408465485,
+          18.800212564350304
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "สวนสนบ่อแก้ว",
+        "type": "natural",
+        "level": "💗💗💗💗💗"
+      },
+      "geometry": {
+        "coordinates": [
+          98.38854300000008,
+          18.156388335748318
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
